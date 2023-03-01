a36a9f7b89481e7ae4ab44f8e058110b587be4b6
Brenda <brenda.solis.arista@gmail.com>
2023-02-28 21:32:14 -0600

    add api

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,11 +1,18 @@
 import MainCard from "../components/shared/MainCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const Pokedex =() =>{
-
-    return (
-<MainCard />
-
-    )
-}
+const Pokedex = () => {
+  const [pokemons, setPokemons] = useState(null);
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20")
+      .then((response) => {
+       setPokemons(response.data.results);
+      });
+  }, []);
+ 
+return <MainCard data={pokemons} />
+};
 
 export default Pokedex;
